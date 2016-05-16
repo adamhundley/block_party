4576d9fde44b0755e60c56ae75f43dfb39f4dc19
Nick Weiland <nickweiland@gmail.com>
2016-05-16 15:30:35 -0600

    change server to express

---
+++ webpack.config.prod.js
@@ -0,0 +1,36 @@
+const path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: [
+    './src/index'
+  ],
+  output: {
+    path: __dirname,
+    filename: "[name].bundle.js"
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
+    })
+  ],
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      { test: /\.css$/, loader: "style!css" },
+      { test: /\.scss$/, loader: "style!css!sass" }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.json', '.scss', '.css']
+  }
+};
