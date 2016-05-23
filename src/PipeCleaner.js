cc47f3bbe86c182d7421b2d2063133a0be7b5aba
Nick Weiland <nickweiland@gmail.com>
2016-05-23 17:08:52 -0600

    remove unessessary duplication

--- src/PipeCleaner.js
+++ src/PipeCleaner.js
@@ -1,39 +1,31 @@
 export default class PipeCleaner {
   constructor(pipe) {
-    this.entryTop = this.pipeUpperBound(pipe);
-    this.entryBottom = this.pipeLowerBound(pipe);
-    this.entranceX = this.pipeEntranceX(pipe);
-    this.exitX = this.pipeExitX(pipe);
-    this.pipeColor = this.pipeColor(pipe);
+    if (pipe) {
+      this.entryTop = this.pipeUpperBound(pipe);
+      this.entryBottom = this.pipeLowerBound(pipe);
+      this.entranceX = this.pipeEntranceX(pipe);
+      this.exitX = this.pipeExitX(pipe);
+      this.pipeColor = this.pipeColor(pipe);
+    }
   }
 
   pipeUpperBound(pipe){
-    if (pipe) {
-      return pipe.y;
-    }
+    return pipe.y;
   }
 
   pipeLowerBound(pipe){
-    if (pipe) {
-      return pipe.y + pipe.height;
-    }
+    return pipe.y + pipe.height;
   }
 
   pipeEntranceX(pipe){
-    if (pipe) {
-      return pipe.x;
-    }
+    return pipe.x;
   }
 
   pipeExitX(pipe){
-    if (pipe) {
-      return pipe.x + pipe.width;
-    }
+    return pipe.x + pipe.width;
   }
 
   pipeColor(pipe){
-    if (pipe) {
-      return pipe.color;
-    }
+    return pipe.color;
   }
 }
