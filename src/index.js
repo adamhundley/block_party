5e28449d2ebbb78836d37b8a8bd1669541a78191
Nick Weiland <nickweiland@gmail.com>
2016-05-25 22:45:50 -0600

    functional manifest.json

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BlockParty } from './BlockParty';
 import style from './style.css';
+import json from './manifest.json';
 
 render(<BlockParty title='BlockParty'/>, document.getElementById('root'));
