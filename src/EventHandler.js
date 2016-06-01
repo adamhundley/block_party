7f2f29c86938f4e13083bba5f2f833fc2a5150d5
Adam Hundley <adamhundley@gmail.com>
2016-06-01 15:28:27 -0600

    more changes

--- src/EventHandler.js
+++ src/EventHandler.js
@@ -12,7 +12,7 @@ export default class EventHandler {
   }
 
   handleOrientationChange(game, e) {
-    alert("Change" + screen.orientation)
+    alert("Change" + screen.orientation.type)
     // if(screen.orientation.type === "portrait-primary" || screen.orientation.type === "portrait-secondary"){
     //   game.pauseGame();
     // } else {
